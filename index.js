960db6ca0befd404798dffaabaa553274cefe0f5
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-04-08 16:45:20 -0600

    Done.

--- index.js
+++ index.js
@@ -15,20 +15,46 @@ function mapToSquare(sourceArray){
     return sourceArray.map(element => element ** 2)
 }
 
-function reduceToTotal(sourceArray, startingPoint=0) {
-    return sourceArray.reduce(getSum, startingPoint)
+// function reduceToTotal(sourceArray, startingPoint=0) {
+//     return sourceArray.reduce(getSum, startingPoint)
+// }
+
+// function getSum(total, num) {
+//     return total + num
+// }
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    let total = startingPoint
+    for(let i = 0; i < sourceArray.length; i++) {
+           total += sourceArray[i] 
+    }
+    return total
 }
 
-function getSum(total, num) {
-    return total + num
-}
+
+// function reduceToAllTrue(sourceArray){
+//     const value = sourceArray.find(element => element === false)
+//     if (value === undefined) { return true } else { return false }
+// }
 
 function reduceToAllTrue(sourceArray){
-    const value = sourceArray.find(element => element === false)
-    if (value === undefined) { return true } else { return false }
+    let a = []
+    for(let i = 0; i < sourceArray.length; i ++){
+        if (!!sourceArray[i] === true){} else {return false}
+    }
+    return true
 }
 
+// function reduceToAnyTrue(sourceArray){
+//     const value = sourceArray.find(element => element === true)
+//     if (value === undefined) { return false } else { return true }
+// }
+
 function reduceToAnyTrue(sourceArray){
-    const value = sourceArray.find(element => element === true)
-    if (value === undefined) { return false } else { return true }
+    let a = []
+    for(let i = 0; i < sourceArray.length; i ++){
+        if (!!sourceArray[i] === false){} else {return true}
+    }
+    return false
 }
+
